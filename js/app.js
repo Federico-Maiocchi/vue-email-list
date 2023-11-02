4ef9ff3b5b56c8d9fc1336a9461d8d970a3ab025
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-02 14:19:03 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// console.log('Hello JS');
+
+const {createApp} = Vue;
+
+createApp ({
+    data () {
+        return {
+            
+        }
+    },
+
+    methods: {
+
+    },
+
+    mounted() {
+        console.log('Hello Vue');
+    }
+
+}).mount('#app')
