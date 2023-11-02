b644b44f992096bf4e71fd6bf7464a9b508ad5fd
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-02 14:56:21 +0100

    ADD: axios js vanilla

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const {createApp} = Vue;
 createApp ({
     data () {
         return {
-            
+
         }
     },
 
@@ -18,3 +18,17 @@ createApp ({
     }
 
 }).mount('#app')
+
+// Axios in Js Vanilla
+
+axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((res) => {
+
+        // res contiene la risposta totale del server
+        console.log(res);
+        // res.data contiene il Json convertito in oggetto
+        console.log(res.data);
+        // res.data.response contiene la proprietà dell'oggetto del data
+        console.log(res.data.response);
+    })
